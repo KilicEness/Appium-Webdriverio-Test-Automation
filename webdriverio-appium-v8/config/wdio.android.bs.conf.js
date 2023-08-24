35786ba866764179b25ac9a785ba1120c1d73a67
enes <eneskiliicc06@gmail.com>
2023-08-24 16:34:46 +0300

    browserstack integration

---
+++ webdriverio-appium-v8/config/wdio.android.bs.conf.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const path = require('path');
+const { config } = require('./wdio.shared.conf');
+
+config.user = process.env.BROWSERSTACK_USER;
+config.key = process.env.BROWSERSTACK_KEY;
+
+config.specs = [
+    // ToDo: define location for spec files here
+    path.join(process.cwd(), './test/specs/android/add-note-screen*.js')
+];
+
+// ============
+// Capabilities
+// ============
+
+config.capabilities = [{
+    platformName: 'Android',
+    'appium:platformVersion': '9.0',
+    'appium:deviceName': 'Google Pixel 3',
+    'appium:automationName': 'UIAutomator2',
+    'appium:app': "bs://fc7b12bb24404e003b284aa4edb6623fde9750a0",
+    'appium:autoGrantPermissions': true
+}];
+
+// Test runner services
+// Services take over a specific job you don't want to take care of. They enhance
+// your test setup with almost no effort. Unlike plugins, they don't add new
+// commands. Instead, they hook themselves up into the test process.
+config.services = ['browserstack'];
+
+exports.config = config;
--- webdriverio-appium-v8/config/wdio.shared.conf.js
+++ webdriverio-appium-v8/config/wdio.shared.conf.js
@@ -90,11 +90,6 @@ exports.config = {
     // Default request retries count
     connectionRetryCount: 3,
     //
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -118,7 +113,7 @@ exports.config = {
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: ['spec'],
 
-    
+
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
